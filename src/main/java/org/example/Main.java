2f0f517771d7bee52435ace781561054ee61586c
valyi-sandor-nye <urugvalyi@gmail.com>
2025-09-17 13:10:59 +0200

    előtag magyar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(a()+b());
     }
-    public static String a() {return "Hello, ";}
+    public static String a() {return "Szia, ";}
     public static String b() {return "World ";}
 
 }
